7d98ec9c30629781d6550390de985a0a4e180b6a
Isioma <isiomaenuonye@gmail.com>
2023-06-29 07:43:37 -0700

    task 6

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,38 +1,14 @@
-class Building {
-  constructor(sqft) {
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
-
-// Example of a class extending Building
-class House extends Building {
-  constructor(sqft, bedrooms) {
-    super(sqft);
-    this._bedrooms = bedrooms;
-  }
-
-  get bedrooms() {
-    return this._bedrooms;
-  }
-
-  evacuationWarningMessage() {
-    return 'Evacuate the house immediately!';
-  }
-}
-
-// Creating an instance of the House class
-const myHouse = new House(1500, 3);
-
-// Accessing the attributes and methods
-console.log(myHouse.sqft); // Output: 1500
-console.log(myHouse.bedrooms); // Output: 3
-console.log(myHouse.evacuationWarningMessage()); // Output: "Evacuate the house immediately!"
-
