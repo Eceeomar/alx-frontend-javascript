22413aee74c7cd7736cfc16772672f57546e06dd
Isioma <isiomaenuonye@gmail.com>
2023-06-29 00:25:19 -0700

    updated the code to handle multiple request

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,26 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+  const promises = [];
+
+  promises.push(signUpUser(firstName, lastName));
+  promises.push(uploadPhoto(fileName));
+
+  const results = [];
+
+  return Promise.allSettled(promises)
+    .then((settledPromises) => {
+      for (let i = 0; i < settledPromises.length; i += 1) {
+        const result = settledPromises[i];
+        const { status } = result;
+        const value = status === 'fulfilled' ? result.value : result.reason;
+
+        results.push({
+          status,
+          value,
+        });
+      }
+
+      return results;
+    });
 }
